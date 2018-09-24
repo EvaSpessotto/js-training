754f396acf3bf54833e44b6ff7664fcd1270d314
Eva Spessotto <eva@spessotto.com>
2018-09-24 15:29:58 +0200

    Correction multiply

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -10,25 +10,36 @@
 
 // Your code :
 
-// function multiply(num1, num2){
-//     let sum = 0;
-//     for (let count = 0; count < num2; count ++){
-//         sum += num1;
-//     }
-//     return(sum);
-// }
-
 function multiply(num1, num2){
     let sum = 0;
-    let count = 0
-    while (count > num2){
-        sum += num1;
+    if (num1 === 0 || num2 === 0){
+        return 0;
     }
-    return(sum);
+    else if (num1 > 0 && num2 > 0){
+        for (let i = 0; i < num2; i++){
+            sum += num1;
+        }
+        return sum;
+    }
+    else if (num1 < 0){
+        for (let j = 0; j > num1; j--){
+            sum += num2;
+        }
+        return -sum;
+    }
+    else if (num2 < 0){
+        for (let j = 0; j > num2; j--){
+            sum += num1;
+        }
+        return -sum;
+    }
+    else if (num1 < 0 && num2 < 0){
+        for (let k = 0; k > num1; k++){
+            sum += num2;
+        }
+        return -sum;
+    }
 }
-
-
-
 //* Begin of tests
 const assert = require('assert')
 
@@ -44,4 +55,4 @@ assert.strictEqual(multiply(0, 0), 0)
 assert.strictEqual(multiply(123, -22), -2706)
 assert.strictEqual(multiply(-22, 123), -2706)
 assert.strictEqual(multiply(-22, -123), 2706)
-// End of tests */
+// End of tests*/
